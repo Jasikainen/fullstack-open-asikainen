3728117d5d8a4b17790179162e561a69548892f4
Jasikainen <jani.aasikainen@gmail.com>
2021-01-04 16:42:12 +0200

    exercise 1.14 anekdootit step3

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,24 +1,80 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+// To render a simple button with event listener and text
 const Button = (props) => {
   const { handleClick, text } = props;
   return <button onClick={handleClick}>{text}</button>;
 };
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0);
-  // Button to set random
-
-  const handleSelected = () => {
-    const randNum = Math.trunc(Math.random() * anecdotes.length);
-    console.log(randNum);
-    setSelected(randNum);
-  };
+// Render current anecdotes votes
+const CurrentAnecdoteStatistic = (props) => {
+  const { currentAnectode, anecdoteVoteList } = props;
   return (
     <div>
-      <Button handleClick={handleSelected} text="next anecdote" />
+      <p>has {anecdoteVoteList[currentAnectode]} votes</p>
+    </div>
+  );
+};
+const TopAnecdote = (props) => {
+  const { anecdoteVoteList, anecdotes } = props;
+
+  // Find the max voted anectode of all available and its idx
+  var maxAnecdote = anecdotes[0];
+  var maxVotes = 0;
+  var maxIndex = 0;
+  for (var i = 0; i < anecdoteVoteList.length; ++i) {
+    if (anecdoteVoteList[i] > maxVotes) {
+      // Update the max votes and its corresponding anecdote
+      maxVotes = anecdoteVoteList[i];
+      maxAnecdote = anecdotes[i];
+      maxIndex = i;
+    }
+  }
+
+  if (maxVotes === 0) {
+    return <div>No anecdote has votes yet!</div>;
+  }
+
+  return (
+    <div>
+      <p>{maxAnecdote}</p>
+      <p>has {anecdoteVoteList[maxIndex]} votes</p>
+    </div>
+  );
+};
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVote] = useState(new Uint8Array(anecdotes.length));
+
+  // Even handler for random button
+  const handleSelected = () => {
+    const randNum = Math.trunc(Math.random() * anecdotes.length);
+    setSelected(randNum);
+  };
+
+  // Event handler for vote button
+  const handleVoted = () => {
+    var copy = [...votes];
+    copy[selected] += 1;
+    setVote(copy);
+  };
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
       <p>{props.anecdotes[selected]}</p>
+
+      <Button handleClick={handleSelected} text="next anecdote" />
+      <Button handleClick={handleVoted} text="vote" />
+      <CurrentAnecdoteStatistic
+        currentAnectode={selected}
+        anecdoteVoteList={votes}
+      />
+
+      <h2>Anecdote with most votes</h2>
+      <TopAnecdote anecdoteVoteList={votes} anecdotes={anecdotes} />
     </div>
   );
 };
