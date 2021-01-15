c6fc1c2475408a69e5b2de2b0a1c543e436ba58f
Jasikainen <jani.aasikainen@gmail.com>
2021-01-15 11:53:02 +0200

    exercise 2.19 step 11 of phonebook

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,7 +2,29 @@ import React, { useState, useEffect } from "react";
 import Person from "./components/Person";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import Notification from "./components/Notification";
 import personService from "./services/persons";
+
+/*
+changeSuccessStatus in brief handles the status shown of add, remove or update
+*/
+const changeSuccessStatus = (personObject, operation, setSuccessMessage) => {
+  if (operation === "add" || operation === "remove") {
+    console.log(operation)
+    setSuccessMessage(operation === "add" 
+      ? `Added ${personObject.name}` 
+      : `Removed ${personObject.name}`)
+    setTimeout(() => {
+      setSuccessMessage(null)
+    }, 2000)
+  }
+  else if (operation === "update"){
+    setSuccessMessage(`Changed ${personObject.name}'s number to ${personObject.number}`)
+    setTimeout(() => {
+      setSuccessMessage(null)
+    }, 2000)
+  }
+};
 
 
 const App = () => {
@@ -10,6 +32,7 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
+  const [successMessage, setSuccessMessage] = useState(null)
 
   useEffect(() => {
     console.log("effect");
@@ -33,7 +56,7 @@ const App = () => {
     };
  
     if (personFound) {
-      // Replace the persons phonenumber if they want to.
+      // Replace the persons phonenumber if user accepts confirm.
       if (window.confirm(`${personFound.name} is already found, replace the old number
        with a new one?`)){
         const person = persons.find(n => n.name === newName)
@@ -41,8 +64,11 @@ const App = () => {
         personService
           .update(id, personObject)
           .then(returnedPerson => {
-            setPersons(persons.map(person => person.id !== id
-               ? person : returnedPerson))
+            setPersons(persons.map(person => 
+              person.id !== id
+               ? person 
+               : returnedPerson))
+            changeSuccessStatus(personObject, "update", setSuccessMessage)
           })
           .catch(error => {
             alert(
@@ -57,13 +83,13 @@ const App = () => {
       personService
         .create(personObject)
         .then(returnedPerson => {
-        setPersons(persons.concat(returnedPerson))
+          setPersons(persons.concat(returnedPerson))
+          changeSuccessStatus(personObject, "add", setSuccessMessage)
       })
     }
     setNewName("")
     setNewNumber("")
   };
-
 
   const deletePerson = id => {
     const person = persons.find(n => n.id === id)
@@ -72,13 +98,13 @@ const App = () => {
           .del(id)
           .then(returnedPerson => {
             setPersons(persons.filter((item) => item.id !== id));
+            changeSuccessStatus(person, "remove", setSuccessMessage)
           })
           .catch((error) => {
             alert(`status error: ${error.status}. Person not found.`);
         })
     }   
   }
-
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -90,10 +116,10 @@ const App = () => {
     setFilterName(event.target.value)
   };
 
-  
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={successMessage} />
       <Filter filterName={filterName} handleFilterChange={handleFilterChange} />
 
       <h3>Add a new</h3>
