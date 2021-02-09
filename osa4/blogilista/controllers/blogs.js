6335fdd0fce20896e0a40839761f6d71f888a9c6
Jasikainen <jani.aasikainen@gmail.com>
2021-02-09 20:51:57 +0200

    fixed the 'bug' on 4.17 exercise that caused blogs not to be visible on api/users

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -4,50 +4,50 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  // request must have valid body to continue
   if (body.url === undefined || body.title === undefined) {
     return response.status(400).end()
   }
 
-  const token = request.token // Middleware handles token extraction
+  try {
+    const token = request.token // Middleware handles token extraction
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response
+        .status(401)
+        .json({ error: 'token is missing or invalid' })
+    }
 
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
-    return response
-      .status(401)
-      .json({ error: 'token is missing or invalid' })
+    const user = await User.findById(decodedToken.id)
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+      !== undefined
+        ? body.likes
+        : 0,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+
+    await user.save()
+    response.json(savedBlog)
+
+  } catch(exception) {
+    next(exception)
   }
 
-  const user = await User.findById(decodedToken.id)
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-      !== undefined
-      ? body.likes
-      : 0,
-    user: user.id
-  })
-
-  const savedBlog = await blog.save()
-  console.log("savedBlog", savedBlog)
-  user.blogs = user.blogs.concat(savedBlog.id)
-  console.log("user.blogs", user.blogs)
-  console.log("await User.findById(decodedToken.id)", await User.findById(decodedToken.id))
-  response.json(savedBlog.toJSON())
 })
-
 
 blogsRouter.delete('/:id', async (request, response) => {
   const databaseResponse = await Blog.findByIdAndRemove(request.params.id)
