ccc38ffe9dc5dd84a90fea11c8aa525fe32108c8
Jasikainen <jani.aasikainen@gmail.com>
2021-02-17 13:01:25 +0200

    exercise 5.7* ability to view blog information and like button for likes++

--- osa4/blog-frontend/src/components/Blog.js
+++ osa4/blog-frontend/src/components/Blog.js
@@ -1,23 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Blog = ({ blog, blogUser }) => {
   const titleStyle = {
-    fontWeight: "500",
+    fontWeight: "350",
     fontSize: 15,
     border: "solid",
     borderWidth: 1,
-    paddingBottom: 10,
+    paddingBottom: 5,
     paddingTop: 10,
     paddingLeft: 5,
     backgroundColor: 'rgba(240, 180, 150, .5)'
   }
 
+  const [viewInfo, setViewInfo] = useState(false)
+  const hideWhenVisible = { display: viewInfo ? 'none' : '' }
+  const showWhenVisible = { 
+    display: viewInfo ? '' : 'none',
+    fontWeight: "500",
+    border: "solid",
+    borderColor: 'rgba(0, 0, 0, .5)',
+    backgroundColor: 'rgba(240, 180, 150, .6)',
+    width: 300,
+    padding: 5
+    }
+  const toggleVisibility = () => {
+    setViewInfo(!viewInfo)
+  }
+
+  // Set like button red for a while after liking
+  const [color, setColor] = useState(false)
+  const showColor = { 
+    color: color ? 'red' : ''
+  }
+  const toggleColor = () => {
+    setColor(true)
+    setTimeout(() => {
+      setColor(false)
+    }, 500)
+  }
+
+  // Blogs have button to display/hide information about them
   return (
     <div style={titleStyle}>
-      {blog.title} {" - "} {blog.author}
-    </div>
-  )
 
+      <div style={hideWhenVisible}>
+        {blog.title} {" - "} {blog.author} <button onClick={toggleVisibility}>View information</button>
+      </div>
+
+      <div style={showWhenVisible}>
+          <button onClick={toggleVisibility}>Hide information</button>
+          <div>Title: {blog.title}</div>
+          <div>Url: {blog.url}</div>
+          <div>
+            likes: {blog.likes} <button style={showColor} onClick={toggleColor}>Like</button>
+          </div>
+          <div>Author: {blog.author}</div>
+          <div>Id: {blog.id}</div>
+      </div>
+
+  </div>
+  )
 }
+
 export default Blog
